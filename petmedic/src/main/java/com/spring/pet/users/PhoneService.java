c8d79e1466333ba8738b6cc21772725b34a18502
subin-git <131166334+Subingitup@users.noreply.github.com>
2023-09-19 09:00:49 +0900

    Update PhoneService.java
    0919

--- petmedic/src/main/java/com/spring/pet/users/PhoneService.java
+++ petmedic/src/main/java/com/spring/pet/users/PhoneService.java
@@ -12,15 +12,15 @@ import net.nurigo.java_sdk.exceptions.CoolsmsException;
 public class PhoneService {
     
 	public void certifiedPhoneNumber(String users_tel, String numStr) {
-	       String api_key = "NCSAZJYGRDSMI1BL";
-	       String api_secret = "JLJDL3BZTZJMYXTMK812L4MNYQO1BCI5";
+	       String api_key = "";
+	       String api_secret = "5";
 	       Message coolsms = new Message(api_key, api_secret);
 
 	       // 4 params(to, from, type, text) are mandatory. must be filled
 	       HashMap<String, String> params = new HashMap<String, String>();
 	       params.put("to", users_tel);    // 수신전화번호
-//	       params.put("from", "01040881054");    // 발신전화번호. 테스트시에는 발신,수신 둘다 본인 번호로 하면 됨
-	       params.put("from", "01036289543");    // 발신전화번호. 테스트시에는 발신,수신 둘다 본인 번호로 하면 됨
+//	       params.put("from", "");    // 발신전화번호. 테스트시에는 발신,수신 둘다 본인 번호로 하면 됨
+	       params.put("from", "");    // 발신전화번호. 테스트시에는 발신,수신 둘다 본인 번호로 하면 됨
 	       params.put("type", "SMS");
 	       params.put("text", "[펫메딕] 인증번호는" + "["+numStr+"]" + "입니다."); // 문자 내용 입력
 	       params.put("app_version", "test app 1.2"); // application name and version
